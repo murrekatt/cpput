a6bbb80d815373627ab5c06b1c05753ba9e6f450
Tommy Back <tommy.back@iki.fi>
2014-10-04 17:03:25 +0200

    Removed unnecessary test.

--- tests/Test_TestHarness.cpp
+++ tests/Test_TestHarness.cpp
@@ -79,20 +79,5 @@ struct ExampleFixture
 
 TEST_F(ExampleFixture, when_doing_this_that_will_happen_as_a_result)
 {
-  // Arrange
-  // Act
-  // Assert
   ASSERT_EQ(42, e_.value());
-}
-
-/////////////////////////////////////////////////////////////////////////////
-
-TEST(Example, simple_test_of_something_that_should_result_in_something)
-{
-  // Arrange
-  Example e;
-  // Act
-  int value = e.value();
-  // Assert
-  ASSERT_EQ(42, value);
 }
